3910d0d3015f748f8b4f94b2916d824632ab1f81
Roshan K <roshan@rivigroup.com>
2020-04-26 11:50:37 +0530

    Props. States, Life Cycle Methods

---
+++ src/Components/Button.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import "./Button.css";
+
+const Button = (props) => (
+  <button onClick={props.onClick} className="pri-btn">
+    {props.name}
+  </button>
+);
+
+Button.propTypes = {
+  name: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default Button;
--- src/User/login.js
+++ src/User/login.js
@@ -1,17 +1,43 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 import "./login.css";
 
+import Button from "../Components/Button";
+
 const Login = (props) => {
-  console.log(props);
+  const [surp, setSurp] = useState(null);
   return (
     <Fragment>
-      {props.name.map((val) => (
-        <div className="title">
-          This is {val} code. {props.note}.
+      {props.name.map((val, id) => (
+        <div key={id} className="title">
+          <div>
+            This is {val} code. {props.note}.
+          </div>
+          <Button
+            onClick={() => {
+              setSurp(val);
+            }}
+            name={val}
+          />
+          {surp === val && <Surprise val={val} />}
         </div>
       ))}
     </Fragment>
   );
+};
+
+const Surprise = (props) => {
+  const [Name, setName] = useState(null);
+
+  // ComponentDidMount
+  useEffect(() => {
+    setName(props.val.toUpperCase());
+  }, []);
+
+  useEffect(() => {
+    console.log(Name);
+  }, [Name]);
+
+  return <div>Hi {Name}, Surprise</div>;
 };
 
 export default Login;
